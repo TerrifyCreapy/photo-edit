6fe9136efc4040030fc4d3c95b351680e2f5815c
Egor <83498435+TerrifyCreapy@users.noreply.github.com>
2023-07-13 20:13:16 +0300

    made new layout for brightness and color changing

--- src/pages/ImagePage.tsx
+++ src/pages/ImagePage.tsx
@@ -1,163 +1,150 @@
-import { Button, Card, Grid, Slider, Typography } from "@mui/material";
+import {
+    Card,
+    Grid,
+    Slider,
+    AppBar,
+    Toolbar,
+    Typography,
+    IconButton,
+    Button,
+} from "@mui/material";
 import { FC, useRef, useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import ColorsSlider from "../components/ColorsSlider";
 
 interface IImagePage {
     file: File;
 }
 
 const ImagePage: FC<IImagePage> = ({ file }) => {
-    const ref = useRef<HTMLCanvasElement>({} as HTMLCanvasElement);
+    const canvasRef = useRef<HTMLCanvasElement>({} as HTMLCanvasElement);
+    const [params, setSearchParams] = useSearchParams({ brightness: "true" });
     const [brightness, setBrightness] = useState<number>(100);
-    const [rgba, setRGBA] = useState<{
-        r: number;
-        g: number;
-        b: number;
-        a: number;
-    }>({ r: 255, g: 255, b: 255, a: 100 });
+    const [rgb, setRgb] = useState<any>({ r: 255, g: 255, b: 255 });
 
     useEffect(() => {
-        const handleBeforeUnload = (event: any) => {
-            event.preventDefault();
-            event.returnValue = ""; // Необязательно, но можно установить пустую строку, чтобы не выводить текст в уведомлении.
-        };
+        canvasRef.current.style.display = "block";
+        canvasRef.current.style.margin = "0 auto";
+    }, []);
 
-        window.addEventListener("beforeunload", handleBeforeUnload);
-        const context = ref.current?.getContext("2d");
-        if (context) {
-            const image = new Image();
-            image.src = URL.createObjectURL(file);
-            image.image.onload = () => {
-                ref.current.width = image.width;
-                ref.current.height = image.height;
-                context.filter = `brightness(${brightness}%)`;
-                context.drawImage(image, 0, 0);
-                context.globalCompositeOperation = "multiply";
-                context.fillStyle = `rgba(${rgba.r}, ${rgba.g}, ${rgba.b}, ${
-                    rgba.a / 100
-                })`;
-                context.fillRect(0, 0, ref.current.width, ref.current.height);
-            };
+    useEffect(() => {
+        if (canvasRef) {
+            const context = canvasRef.current.getContext("2d");
+            if (context) {
+                const image = new Image();
+                image.src = URL.createObjectURL(file);
+                image.onload = function (this) {
+                    canvasRef.current.width = image.width;
+                    canvasRef.current.height = image.height;
+                    context.filter = `brightness(${brightness}%)`;
+                    context.drawImage(image, 0, 0);
+
+                    context.globalCompositeOperation = "multiply";
+
+                    // Заливка холста желтым цветом
+                    context.fillStyle = `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`;
+                    context.fillRect(
+                        0,
+                        0,
+                        canvasRef.current.width,
+                        canvasRef.current.height,
+                    );
+                };
+            }
         }
-
-        return () => {
-            window.removeEventListener("beforeunload", handleBeforeUnload);
-        };
-    }, [file, brightness, rgba]);
-
-    function addBrightness(value: number) {
-        if (brightness > 200) return;
-        setBrightness(brightness + value);
-    }
-
-    function removeBrightness(value: number) {
-        if (brightness < 15) return;
-        setBrightness(brightness - value);
-    }
-
-    function onChangeR(event: any) {
-        console.log(event.target.value);
-        setRGBA((value) => {
-            return { ...value, r: +event.target.value };
-        });
-    }
-
-    function onChangeG(event: any) {
-        setRGBA((value) => {
-            return { ...value, g: +event?.target?.value };
-        });
-    }
-
-    function onChangeB(event: any) {
-        setRGBA((value) => {
-            return { ...value, b: +event?.target?.value };
-        });
-    }
-
-    function onChangeA(event: any) {
-        setRGBA((value) => {
-            return { ...value, a: +event.target.value };
-        });
-    }
+    }, [brightness, rgb]);
 
     return (
-        <Grid
-            container
-            spacing={0}
-            alignItems="center"
-            justifyContent="center"
-            sx={{
-                width: "100vw",
-                height: "100vh",
-            }}
-        >
+        <>
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <IconButton
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        123
+                    </IconButton>
+                    <Button
+                        sx={{ color: "black" }}
+                        onClick={() => setSearchParams({ brightness: "true" })}
+                    >
+                        Brightness
+                    </Button>
+                    <Button
+                        sx={{ color: "black" }}
+                        onClick={() => setSearchParams({ color: "true" })}
+                    >
+                        Color
+                    </Button>
+                </Toolbar>
+            </AppBar>
             <Grid
-                item
+                container
+                spacing={0}
+                alignItems="center"
+                justifyContent="center"
                 sx={{
-                    maxWidth: "50vw",
-                    maxHeight: "60vh",
-                    width: "100%",
-                    height: "100%",
+                    width: "100vw",
+                    height: "100vh",
                 }}
             >
-                <Card
+                <Grid
+                    item
                     sx={{
+                        maxWidth: "80vw",
+                        maxHeight: "80vh",
                         width: "100%",
                         height: "100%",
-                        position: "relative",
-                        padding: 2,
                     }}
                 >
-                    <canvas
-                        ref={ref}
-                        style={{
-                            maxWidth: "75%",
-                            maxHeight: "75%",
-                            position: "absolute",
-                            top: 0,
-                            right: 0,
+                    <Card
+                        sx={{
+                            width: "100%",
+                            height: "100%",
+                            position: "relative",
+                            padding: 2,
                         }}
-                    ></canvas>
-                    <Typography component="div">
-                        <Button onClick={() => addBrightness(5)}>
-                            + 5 brightness
-                        </Button>
-                        <Button onClick={() => removeBrightness(5)}>
-                            - 5 brightness
-                        </Button>
-                    </Typography>
-                    <Typography component="div" maxWidth={"30%"}>
-                        <Slider
-                            aria-label="Red"
-                            value={rgba.r}
-                            onChange={onChangeR}
-                            min={0}
-                            max={255}
-                        />
-                        <Slider
-                            aria-label="Green"
-                            value={rgba.g}
-                            onChange={onChangeG}
-                            min={0}
-                            max={255}
-                        />
-                        <Slider
-                            aria-label="Blue"
-                            value={rgba.b}
-                            onChange={onChangeB}
-                            min={0}
-                            max={255}
-                        />
-                        <Slider
-                            aria-label="Alpha"
-                            value={rgba.a}
-                            onChange={onChangeA}
-                            min={0}
-                            max={100}
-                        />
-                    </Typography>
-                </Card>
+                    >
+                        <canvas
+                            style={{ maxWidth: "100%", maxHeight: "80%" }}
+                            ref={canvasRef}
+                        ></canvas>
+                        {params.get("color") && (
+                            <ColorsSlider
+                                rgb={rgb}
+                                onChangeB={(value: number) =>
+                                    setRgb((e: any) => {
+                                        return { ...e, b: value };
+                                    })
+                                }
+                                onChangeR={(value: number) =>
+                                    setRgb((e: any) => {
+                                        return { ...e, r: value };
+                                    })
+                                }
+                                onChangeG={(value: number) =>
+                                    setRgb((e: any) => {
+                                        return { ...e, g: value };
+                                    })
+                                }
+                            />
+                        )}
+                        {params.get("brightness") && (
+                            <Slider
+                                min={30}
+                                max={200}
+                                value={brightness}
+                                onChange={(e: any) =>
+                                    setBrightness(+e.target.value)
+                                }
+                            />
+                        )}
+                    </Card>
+                </Grid>
             </Grid>
-        </Grid>
+        </>
     );
 };
 export default ImagePage;
